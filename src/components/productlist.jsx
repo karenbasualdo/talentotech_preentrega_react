de8051ed6bc898cdf8955efc297d4fc0f48e902a
karenbasualdo <128748358+karenbasualdo@users.noreply.github.com>
2025-12-15 15:53:48 +0000

    Usando mockapi

--- src/components/productlist.jsx
+++ src/components/productlist.jsx
@@ -3,20 +3,30 @@ import { Row, Col } from "react-bootstrap";
 import ProductCard from "./productcard";
 import { useCarrito } from "./CarritoContext";
 
-const ProductList = ({ category }) => {
+const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [busqueda, setBusqueda] = useState("");
+  const [loading, setLoading] = useState(true);
+
   const { agregarAlCarrito } = useCarrito();
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
+    fetch("https://693f647d12c964ee6b6fcb0e.mockapi.io/products")
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) => {
+        setProducts(data);
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
   }, []);
 
   const filtrados = products.filter((p) =>
     p.title.toLowerCase().includes(busqueda.toLowerCase())
   );
+
+  if (loading) {
+    return <p>Cargando productos...</p>;
+  }
 
   return (
     <>
@@ -29,6 +39,8 @@ const ProductList = ({ category }) => {
       />
 
       <Row>
+        {filtrados.length === 0 && <p>No hay productos</p>}
+
         {filtrados.map((product) => (
           <Col md={4} key={product.id} className="mb-4">
             <ProductCard
